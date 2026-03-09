06a8f85a3363a7465c2f50062709e9da41793cf9
MihaiSvn <mihaialexandrusavin@gmail.com>
2026-03-09 12:29:11 +0200

    added custom logic

---
+++ lib/util/input_page/custom_prompts_form.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class CustomPromptsForm extends StatefulWidget {
+  final int minVal;
+  final int maxVal;
+  final List<Map<String, TextEditingController>> controller;
+  const CustomPromptsForm({
+    super.key,
+    required this.minVal,
+    required this.maxVal,
+    required this.controller,
+  });
+
+  @override
+  State<CustomPromptsForm> createState() => _CustomPromptsFormState();
+}
+
+class _CustomPromptsFormState extends State<CustomPromptsForm> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _addRow() {
+    setState(() {
+      widget.controller.add({
+        'left': TextEditingController(),
+        'right': TextEditingController(),
+      });
+    });
+    Future.delayed(const Duration(milliseconds: 100), () {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2.0),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Column(
+        spacing: 17,
+        children: [
+          const Text(
+            "Add Custom Prompts",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+
+          SizedBox(
+            height: 200,
+            child: Scrollbar(
+              controller: _scrollController,
+              thumbVisibility: true,
+              thickness: 6,
+              radius: const Radius.circular(10),
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: Column(
+                  spacing: 12,
+                  children: [
+                    ...widget.controller.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      var controllers = entry.value;
+
+                      return Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 214, 214, 214),
+                          //border: Border.all(color: Colors.black, width: 2.0),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: 12,
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.black,
+                              child: Text(
+                                "${index + 1}",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: TextField(
+                                maxLength: widget.maxVal,
+                                controller: controllers['left'],
+                                decoration: InputDecoration(
+                                  hintText: 'Left prompt',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            Expanded(
+                              child: TextField(
+                                maxLength: widget.maxVal,
+                                controller: controllers['right'],
+                                decoration: InputDecoration(
+                                  hintText: 'Right prompt',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          IconButton(onPressed: _addRow, icon: Icon(Icons.add)),
+        ],
+      ),
+    );
+  }
+}
--- lib/util/input_page/game_form.dart
+++ lib/util/input_page/game_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wavelength/services/prompts_service.dart';
 import 'package:wavelength/util/alert_popup.dart';
+import 'package:wavelength/util/input_page/custom_prompts_form.dart';
 import 'package:wavelength/util/input_page/digit_form.dart';
 import 'package:wavelength/models/game_settings.dart';
 import 'package:wavelength/util/input_page/player_name_form.dart';
@@ -20,6 +21,10 @@ class _GameFormState extends State<GameForm> {
   TextEditingController controllerRounds = TextEditingController();
   TextEditingController controllerSwaps = TextEditingController();
 
+  List<Map<String, TextEditingController>> customPromptsController = [
+    {'left': TextEditingController(), 'right': TextEditingController()},
+  ];
+
   int minRounds = 1;
   int maxRounds = 20;
 
@@ -32,18 +37,45 @@ class _GameFormState extends State<GameForm> {
     String name2 = controllerPlayer2.text;
 
     if (name1.isNotEmpty && name2.isNotEmpty) {
-      final numberRounds = int.tryParse(controllerRounds.text);
-      if (numberRounds == null ||
-          numberRounds < minRounds ||
-          numberRounds > maxRounds) {
-        ok = false;
-        AlertPopup.showPopup(
-          context,
-          title: "Alert",
-          text:
-              "You must set the number of round to a numeric value between $minRounds and $maxRounds!",
-        );
+      if (widget.gameMode == "Classic") {
+        final numberRounds = int.tryParse(controllerRounds.text);
+        if (numberRounds == null ||
+            numberRounds < minRounds ||
+            numberRounds > maxRounds) {
+          ok = false;
+          AlertPopup.showPopup(
+            context,
+            title: "Alert",
+            text:
+                "You must set the number of round to a numeric value between $minRounds and $maxRounds!",
+          );
+        }
+      } else if (widget.gameMode == "Custom") {
+        if (customPromptsController.isEmpty) {
+          ok = false;
+          AlertPopup.showPopup(
+            context,
+            title: "Alert",
+            text: "You must add at least one custom prompt!",
+          );
+        } else {
+          for (var row in customPromptsController) {
+            String leftText = row['left']!.text.trim();
+            String rightText = row['right']!.text.trim();
+
+            if (leftText.isEmpty || rightText.isEmpty) {
+              ok = false;
+              AlertPopup.showPopup(
+                context,
+                title: "Alert",
+                text: "All custom prompts must have both sides filled!",
+              );
+              break;
+            }
+          }
+        }
       }
+
       final numberSwaps = int.tryParse(controllerSwaps.text);
       if (numberSwaps == null ||
           numberSwaps < minSwaps ||
@@ -71,13 +103,25 @@ class _GameFormState extends State<GameForm> {
         if (widget.gameMode == 'Classic') {
           fetchedPrompts = await promptsService.fetchPrompts();
           fetchedPrompts.shuffle();
+        } else if (widget.gameMode == "Custom") {
+          fetchedPrompts = customPromptsController.map((row) {
+            return {
+              "id":
+                  DateTime.now().millisecondsSinceEpoch +
+                  customPromptsController.indexOf(row),
+              "left": row['left']!.text.trim(),
+              "right": row['right']!.text.trim(),
+            };
+          }).toList();
         }
-        if(!mounted) return;
+        if (!mounted) return;
 
         final settings = GameSettings(
           player1Name: name1,
           player2Name: name2,
-          totalRounds: int.parse(controllerRounds.text),
+          totalRounds: widget.gameMode == "Custom"
+              ? fetchedPrompts.length
+              : int.parse(controllerRounds.text),
           turnsPerPrompt: int.parse(controllerSwaps.text),
           mode: widget.gameMode,
           allPrompts: fetchedPrompts,
@@ -101,45 +145,57 @@ class _GameFormState extends State<GameForm> {
     if (dynamicMaxLength < 7) dynamicMaxLength = 7;
     if (dynamicMaxLength > 20) dynamicMaxLength = 20;
 
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: PlayerNameForm(
-                playerIndex: 1,
-                maxLength: dynamicMaxLength,
-                controller: controllerPlayer1,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        spacing: 17,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: PlayerNameForm(
+                  playerIndex: 1,
+                  maxLength: dynamicMaxLength,
+                  controller: controllerPlayer1,
+                ),
               ),
-            ),
-            const SizedBox(width: 20),
-            Expanded(
-              child: PlayerNameForm(
-                playerIndex: 2,
-                maxLength: dynamicMaxLength,
-                controller: controllerPlayer2,
+              const SizedBox(width: 20),
+              Expanded(
+                child: PlayerNameForm(
+                  playerIndex: 2,
+                  maxLength: dynamicMaxLength,
+                  controller: controllerPlayer2,
+                ),
               ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            DigitForm(
-              minVal: minRounds,
-              maxVal: maxRounds,
-              label: "Number of rounds",
-              controller: controllerRounds,
-            ),
-            DigitForm(
-              minVal: minSwaps,
-              maxVal: maxSwaps,
-              label: "Swaps per prompt",
-              controller: controllerSwaps,
-            ),
-          ],
-        ),
-        ElevatedButton(onPressed: validateInput, child: Text("Start game")),
-      ],
+            ],
+          ),
+          Column(
+            spacing: 17,
+            children: [
+              if (widget.gameMode == "Classic")
+                DigitForm(
+                  minVal: minRounds,
+                  maxVal: maxRounds,
+                  label: "Number of rounds",
+                  controller: controllerRounds,
+                )
+              else
+                CustomPromptsForm(
+                  minVal: minRounds,
+                  maxVal: maxRounds,
+                  controller: customPromptsController,
+                ),
+              DigitForm(
+                minVal: minSwaps,
+                maxVal: maxSwaps,
+                label: "Swaps per prompt",
+                controller: controllerSwaps,
+              ),
+            ],
+          ),
+          ElevatedButton(onPressed: validateInput, child: Text("Start game")),
+        ],
+      ),
     );
   }
 }
